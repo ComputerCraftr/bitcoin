de8b18b3ded6b9e25ae1b0e752d5931b930a4987
John Studnicka <ComputerCraftr@users.noreply.github.com>
2021-02-26 04:21:06 -0700

    Add GUI option to hide orphaned coinstakes

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -321,3 +321,9 @@ void OverviewPage::showOutOfSyncWarning(bool fShow)
     ui->labelWalletStatus->setVisible(fShow);
     ui->labelTransactionsStatus->setVisible(fShow);
 }
+
+void OverviewPage::setOrphansHidden(bool hidden)
+{
+    if (filter)
+        filter->setHideOrphans(hidden);
+}
