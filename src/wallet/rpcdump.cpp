6d23b9d177c074103a8f317d6cd872ed5f204f81
John Studnicka <ComputerCraftr@users.noreply.github.com>
2021-07-17 09:42:13 -0700

    Add multisig data/replay as standard

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -901,7 +901,10 @@ static std::string RecurseImportData(const CScript& script, ImportData& import_d
         import_data.import_scripts.emplace(*subscript);
         return RecurseImportData(*subscript, import_data, ScriptContext::P2SH);
     }
-    case TxoutType::MULTISIG: {
+    case TxoutType::MULTISIG:
+    case TxoutType::MULTISIG_REPLAY:
+    case TxoutType::MULTISIG_DATA:
+    case TxoutType::MULTISIG_DATA_REPLAY: {
         for (size_t i = 1; i + 1< solverdata.size(); ++i) {
             CPubKey pubkey(solverdata[i].begin(), solverdata[i].end());
             import_data.used_keys.emplace(pubkey.GetID(), false);
